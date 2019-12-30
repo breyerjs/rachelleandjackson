bed33748b6fa8b791552f5b30f8d2630a96fa58a
breyerjs <breyerjs@yelp.com>
2019-12-30 10:47:42 -0500

    Move foter to the bottom; en-biggen section headers

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -44,8 +44,8 @@ export default class Layout extends React.Component {
                 {gettingThere}
                 {registry}
                 {aboutUs}
-                <Footer />
                 {photo_gallery}
+                <Footer />
             </div>
         )
     }
