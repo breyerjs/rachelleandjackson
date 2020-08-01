9713bc87aef42a7c3bbcbe0c5011eb6e45ec8ae1
breyerjs <breyer.js@gmail.com>
2020-08-01 10:53:46 -0700

    Reschedule wedding

--- src/Program/Program.js
+++ src/Program/Program.js
@@ -13,9 +13,7 @@ export default class Program extends React.Component {
                     <a href="https://www.google.com/maps/place/Barrel+House+Tavern/@37.855213,-122.478827,15z/data=!4m2!3m1!1s0x0:0x4c410f3ca71bcd9e?sa=X&ved=2ahUKEwiWwIzwtpDnAhXJtp4KHWEpCXwQ_BIwHXoECBkQCA
                     " target="_blank">Barrel House Tavern</a>
                     <br />
-                    <span className={styles.strikeThrough}>Friday, August 21, 2020</span>
-                    <br />
-                    <span className={styles.strikeThrough}>6 pm to 9 pm</span>
+                    Date Forthcoming
                     <br />
                     660 Bridgeway, Sausalito, CA 94965
                 </SectionWithTitle>
@@ -23,7 +21,7 @@ export default class Program extends React.Component {
                 <SectionWithTitle title={"ceremony and reception"}>
                     <a href="https://goo.gl/maps/Q27RgUc4VqkVPvuz9" target="_blank">Headlands Center for the Arts</a>
                     <br />
-                    <span className={styles.strikeThrough}>Saturday, August 22, 2020</span>
+                    Saturday August 14, 2021
                     <br />
                     944 Simmonds Rd, Sausalito, CA 94965
                     <br />
@@ -32,9 +30,7 @@ export default class Program extends React.Component {
                 <SectionWithTitle title={"farewell brunch"}>
                     <a href="https://www.google.com/maps/place/The+Spinnaker/@37.8588382,-122.4782071,15z/data=!4m2!3m1!1s0x0:0xa0ec245896bce8cb?sa=X&ved=2ahUKEwiaiv6d1KXnAhUqJTQIHclBBTYQ_BIwFXoECCsQCA" target='_blank'>The Spinnaker</a>
                     <br />
-                    <span className={styles.strikeThrough}>Sunday, August 23, 2020</span>
-                    <br />
-                    <span className={styles.strikeThrough}>9 am to 11  am</span>
+                    Date Forthcoming
                     <br />
                     100 Spinnaker Dr, Sausalito, CA 94965
                 </SectionWithTitle>
