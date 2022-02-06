988e3e892f73a991da89ea0a93b8b7c828f3529f
breyerjs <breyer.js@gmail.com>
2022-02-06 10:31:39 -0800

    Update date

--- src/Program/Program.js
+++ src/Program/Program.js
@@ -13,7 +13,7 @@ export default class Program extends React.Component {
                     <a href="https://www.google.com/maps/place/Barrel+House+Tavern/@37.855213,-122.478827,15z/data=!4m2!3m1!1s0x0:0x4c410f3ca71bcd9e?sa=X&ved=2ahUKEwiWwIzwtpDnAhXJtp4KHWEpCXwQ_BIwHXoECBkQCA
                     " target="_blank">Barrel House Tavern</a>
                     <br />
-                    Date Forthcoming
+                    Date TBA
                     <br />
                     660 Bridgeway, Sausalito, CA 94965
                     <br />
@@ -23,7 +23,7 @@ export default class Program extends React.Component {
                 <SectionWithTitle title={"ceremony and reception"}>
                     <a href="https://goo.gl/maps/Q27RgUc4VqkVPvuz9" target="_blank">Headlands Center for the Arts</a>
                     <br />
-                    Saturday August 14, 2021
+                    Saturday June 11, 2022
                     <br />
                     944 Simmonds Rd, Sausalito, CA 94965
                     <br />
@@ -33,7 +33,7 @@ export default class Program extends React.Component {
                 <SectionWithTitle title={"farewell brunch"}>
                     <a href="https://www.google.com/maps/place/The+Spinnaker/@37.8588382,-122.4782071,15z/data=!4m2!3m1!1s0x0:0xa0ec245896bce8cb?sa=X&ved=2ahUKEwiaiv6d1KXnAhUqJTQIHclBBTYQ_BIwFXoECCsQCA" target='_blank'>The Spinnaker</a>
                     <br />
-                    Date Forthcoming
+                    Date TBA
                     <br />
                     100 Spinnaker Dr, Sausalito, CA 94965
                     <br />
