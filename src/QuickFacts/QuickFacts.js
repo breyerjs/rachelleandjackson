49f5bf07aedc2b683e7b38e1c4981cb538c4a3fa
breyerjs <breyer.js@gmail.com>
2022-02-07 17:12:35 -0800

    Consolidate transportation info

--- src/QuickFacts/QuickFacts.js
+++ src/QuickFacts/QuickFacts.js
@@ -7,21 +7,20 @@ export default class QuickFacts extends React.Component {
     render() {
         return (
             <div>
-                <SectionHeader title={"specifics"} />
+                <SectionHeader title={"ceremony specifics"} />
 
-                <SectionWithTitle title={"ceremony transportation"}>
-                    <b>Shuttle schedule:</b> TBA
-                    <br/>
+                <SectionWithTitle title={"transportation"}>
+                    <b>Bus Shuttle:</b> TBA
+                    <br/><br/>
                     <b>Parking:</b> There's parking behind the venue as well as at the <a href="https://goo.gl/maps/VYh8YyxemrQNY31P9" target="_blank">visitor center</a> down the hill.
+                    <br/><br/>
+                    <b>Rideshares:</b> There is no cell service at the venue, so please consider <a href="https://www.uber.com/us/en/ride/how-it-works/scheduled-rides/" target='_blank'>scheduling your rideshare ahead of time</a>.
                 </SectionWithTitle>
-            
+                
                 <SectionWithTitle title={"attire"}>
                     Semi-formal. Think suit and tie or a dress. But honestly, just look nice and you're all set.
                 </SectionWithTitle>
 
-                <SectionWithTitle title={"rideshares"}>
-                    There is no cell service at the venue, so please consider <a href="https://www.uber.com/us/en/ride/how-it-works/scheduled-rides/" target='_blank'>scheduling your rideshare ahead of time</a> if needed.
-                </SectionWithTitle>
 
             </div> 
         )
