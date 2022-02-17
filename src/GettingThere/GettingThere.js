4e10e7fb6e997430e8d9490cb25c6c5444b77eb5
breyerjs <breyer.js@gmail.com>
2022-02-17 09:55:50 -0800

    Add sugg. hotels

--- src/GettingThere/GettingThere.js
+++ src/GettingThere/GettingThere.js
@@ -15,14 +15,61 @@ export default class GettingThere extends React.Component {
                     Oakland International Airport (OAK)
                 </SectionWithTitle>
 
-                <SectionWithTitle title={"places to stay"}>
-                    Suggested Hotels: TBA 
+                <SectionWithTitle title={"where to stay"}>
+                    Please note that a room block is coming soon. But if you want to book now:
+                    <br /><br />
+                    In Marin:
+                    <ul>
+                        <li>
+                            <a href="https://www.ihg.com/holidayinnexpress/hotels/us/en/mill-valley/sfomv/hoteldetail" target='_blank'>Holiday Inn Express Mill Valley</a>
+                        </li>
+                        <li>
+                            <a href="https://acquahotel.com/" target='_blank'>Acqua Hotel</a>
+                        </li>
+                        <li>
+                            <a href="https://millvalleyinn.com/" target='_blank'>Mill Valley Inn</a>
+                        </li>
+                        <li>
+                            <a href="https://www.cavallopoint.com/" target='_blank'>Cavallo Point</a>
+                        </li>
+                    </ul>
+                    In San Francisco:
+                    <ul>
+                        <li>
+                            <a href="http://www.presidiolodging.com/lodge-at-the-presidio/" target='_blank'>The Lodge at Presidio</a>
+                        </li>
+                        <li>
+                            <a href="http://www.presidiolodging.com/inn-at-the-presidio/" target='_blank'>In at the Presidio </a>
+                        </li>
+                        <li>
+                            <a href="https://www.jdvhotels.com/hotels/california/san-francisco/the-laurel-inn" target='_blank'>The Laurel Inn (Pac Heights)</a>
+                        </li>
+                        <li>
+                            <a href="https://metrohotelsf.com/" target='_blank'>The Metro Hotel (Divisadero)</a>
+                        </li>
+                        <li>
+                            <a href="https://becksmotorlodge.com/" target='_blank'>Beck's Motor Lodge (Castro)</a>
+                        </li>
+                        <li>
+                            <a href="https://www.parkerguesthouse.com/" target='_blank'>Parker Guest House (The Mission)</a>
+                        </li>
+                    </ul>
+
+                    Areas to Stay: 
                     <br />
-                    (including a future room block)
-                    <br/><br />
-                    Areas to Stay: Sausalito, Presidio, Mill Valley, Tiburon
-                    <br />
-                    <a href="https://photos.google.com/share/AF1QipOnBpZ2dpq32pDTYb-eNY74EB85PcH8GMLRZZKLMIQij8UcbY4FcPTF2dT_a4wqyQ/photo/AF1QipMepHiItqO9RiFgQeIIqhbaxvvN1gFyGMVvPtYO?key=dkREZllpaWp6Tkdqc2cweElVTmNBem1WakxtRHh3" target="_blank">(here's a map Rachelle drew)</a>
+                    
+                    <ul className={styles.bottomList}>
+                        <li>
+                            Generally, north of Golden Gate Bridge
+                            <a href="https://photos.google.com/share/AF1QipOnBpZ2dpq32pDTYb-eNY74EB85PcH8GMLRZZKLMIQij8UcbY4FcPTF2dT_a4wqyQ/photo/AF1QipMepHiItqO9RiFgQeIIqhbaxvvN1gFyGMVvPtYO?key=dkREZllpaWp6Tkdqc2cweElVTmNBem1WakxtRHh3" target="_blank"> (here's a map Rachelle drew)</a>
+                        </li>
+                        <li>Sausalito</li>
+                        <li>Mill Valley</li>
+                        <li>Tiburon</li>
+                        <li>Presidio</li>
+                        <li>Inner or Outer Richmond</li>
+                        <li>Cole Valley</li>
+                    </ul>
                 </SectionWithTitle>
 
                 <SectionWithTitle title={"weather"}>
@@ -30,11 +77,11 @@ export default class GettingThere extends React.Component {
                 </SectionWithTitle>
 
                 <SectionWithTitle title={"need suggestions?"}>
-                    <a href="https://www.yelp.com/collection/ObkpXubOQAqMAnu4gJrSzQ" target="_blank">Restaurants</a>
-                    <br />
-                    <a href="https://www.yelp.com/collection/rK7LzLK3L5VgzyfqfDixdA" target="_blank">Things to do</a>
-                    <br />
-                    <a href="https://www.yelp.com/collection/052eNjwpCzvL3-1gM_btuQ" target="_blank">Bars</a>
+                    <ul>
+                        <li><a href="https://www.yelp.com/collection/ObkpXubOQAqMAnu4gJrSzQ" target="_blank">Restaurants</a></li>
+                        <li><a href="https://www.yelp.com/collection/rK7LzLK3L5VgzyfqfDixdA" target="_blank">Things to do</a></li>
+                        <li><a href="https://www.yelp.com/collection/052eNjwpCzvL3-1gM_btuQ" target="_blank">Bars</a></li>
+                    </ul>
                 </SectionWithTitle>
 
                 <SectionWithTitle title={"venue history"}>
